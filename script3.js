2190c57c986f660fa90db589a0cb8437916f1fe7
Egormity <kotlaregor9@gmail.com>
2024-11-24 11:04:06 +0700

    beakPeaks

--- script3.js
+++ script3.js
@@ -44,38 +44,53 @@
 // }
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-// function loop_size(node) {
-//     var turtle = node;
-//     var rabbit = node;
+function loop_size(node) {
+    var turtle = node;
+    var rabbit = node;
 
-//     // Find a point in the loop.  Any point will do!
-//     // Since the rabbit moves faster than the turtle
-//     // and the kata guarantees a loop, the rabbit will
-//     // eventually catch up with the turtle.
-//     do {
-//         turtle = turtle.getNext();
-//         rabbit = rabbit.getNext().getNext();
-//     } while (turtle != rabbit);
+    // Find a point in the loop.  Any point will do!
+    // Since the rabbit moves faster than the turtle
+    // and the kata guarantees a loop, the rabbit will
+    // eventually catch up with the turtle.
+    do {
+        turtle = turtle.getNext();
+        rabbit = rabbit.getNext().getNext();
+    } while (turtle != rabbit);
 
-//     // The turtle and rabbit are now on the same node,
-//     // but we know that node is in a loop.  So now we
-//     // keep the turtle motionless and move the rabbit
-//     // until it finds the turtle again, counting the
-//     // nodes the rabbit visits in the mean time.
-//     var count = 0;
-//     do {
-//         ++count;
-//         rabbit = rabbit.getNext();
-//     } while (turtle != rabbit);
+    // The turtle and rabbit are now on the same node,
+    // but we know that node is in a loop.  So now we
+    // keep the turtle motionless and move the rabbit
+    // until it finds the turtle again, counting the
+    // nodes the rabbit visits in the mean time.
+    var count = 0;
+    do {
+        ++count;
+        rabbit = rabbit.getNext();
+    } while (turtle != rabbit);
 
-//     // voila
-//     return count;
-// }
+    // voila
+    return count;
+}
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // const square = (n) => n ** 2;
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function pickPeaks(arr) {
+    const response = { pos: [], peaks: [] };
+
+    for (let i = 1; i < arr.length - 1; i++) {
+        let next = 1;
+        while (arr[i] === arr[i + next]) next++;
+
+        if (arr[i] > arr[i - 1] && arr[i] > arr[i + next]) {
+            response.pos.push(i);
+            response.peaks.push(arr[i]);
+        }
+    }
+
+    return response;
+}
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
