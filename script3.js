33d9bd1ccf743f5864a5b9f3f4e9eb022bdb9187
Egormity <kotlaregor9@gmail.com>
2024-11-26 21:37:31 +0700

    stockList

--- script3.js
+++ script3.js
@@ -44,55 +44,72 @@
 // }
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-function loop_size(node) {
-    var turtle = node;
-    var rabbit = node;
+// function loop_size(node) {
+//     var turtle = node;
+//     var rabbit = node;
 
-    // Find a point in the loop.  Any point will do!
-    // Since the rabbit moves faster than the turtle
-    // and the kata guarantees a loop, the rabbit will
-    // eventually catch up with the turtle.
-    do {
-        turtle = turtle.getNext();
-        rabbit = rabbit.getNext().getNext();
-    } while (turtle != rabbit);
+//     // Find a point in the loop.  Any point will do!
+//     // Since the rabbit moves faster than the turtle
+//     // and the kata guarantees a loop, the rabbit will
+//     // eventually catch up with the turtle.
+//     do {
+//         turtle = turtle.getNext();
+//         rabbit = rabbit.getNext().getNext();
+//     } while (turtle != rabbit);
 
-    // The turtle and rabbit are now on the same node,
-    // but we know that node is in a loop.  So now we
-    // keep the turtle motionless and move the rabbit
-    // until it finds the turtle again, counting the
-    // nodes the rabbit visits in the mean time.
-    var count = 0;
-    do {
-        ++count;
-        rabbit = rabbit.getNext();
-    } while (turtle != rabbit);
+//     // The turtle and rabbit are now on the same node,
+//     // but we know that node is in a loop.  So now we
+//     // keep the turtle motionless and move the rabbit
+//     // until it finds the turtle again, counting the
+//     // nodes the rabbit visits in the mean time.
+//     var count = 0;
+//     do {
+//         ++count;
+//         rabbit = rabbit.getNext();
+//     } while (turtle != rabbit);
 
-    // voila
-    return count;
-}
+//     // voila
+//     return count;
+// }
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // const square = (n) => n ** 2;
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-function pickPeaks(arr) {
-    const response = { pos: [], peaks: [] };
+// function pickPeaks(arr) {
+//     const response = { pos: [], peaks: [] };
 
-    for (let i = 1; i < arr.length - 1; i++) {
-        let next = 1;
-        while (arr[i] === arr[i + next]) next++;
+//     for (let i = 1; i < arr.length - 1; i++) {
+//         let next = 1;
+//         while (arr[i] === arr[i + next]) next++;
 
-        if (arr[i] > arr[i - 1] && arr[i] > arr[i + next]) {
-            response.pos.push(i);
-            response.peaks.push(arr[i]);
-        }
-    }
+//         if (arr[i] > arr[i - 1] && arr[i] > arr[i + next]) {
+//             response.pos.push(i);
+//             response.peaks.push(arr[i]);
+//         }
+//     }
 
-    return response;
-}
-
+//     return response;
+// }
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// const stockList = (books, cts) =>
+//     books.length === 0
+//         ? ""
+//         : Object.entries(
+//               books.reduce(
+//                   (acc, cur) => {
+//                       const toAdd = acc[cur[0]] || 0;
+//                       return { ...acc, [cur[0]]: +cur.split(" ")[1] + toAdd };
+//                   },
+//                   cts.reduce((acc, cur) => ({ ...acc, [cur]: 0 }), {})
+//               )
+//           ).reduce(
+//               (acc, cur, i) =>
+//                   cts.includes(cur[0]) ? acc + `${i === 0 ? "" : " - "}(${cur[0]} : ${cur[1]})` : acc,
+//               ""
+//           );
+
+// console.log(stockList(["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"], ["A", "B", "C", "D"]));
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
