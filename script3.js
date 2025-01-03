3c81639ca22a30de1cdef6075c22c38fb0b25672
Egor Kotlyar <kotlaregor9@gmail.com>
2025-01-03 20:55:59 +0700

    Student's Final Grade 8kuy

--- script3.js
+++ script3.js
@@ -395,6 +395,20 @@
 // }
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// `
+// This function should return a number (final grade). There are four types of final grades:
+
+// 100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+// 90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+// 75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+// 0, in other cases
+// `
+// function finalGrade (e, p) {
+//   if (e > 90 || p > 10) return 100;
+//   if (e > 75 && p >= 5) return 90;
+//   if (e > 50 && p >= 2) return 75;
+//   return 0;
+// }
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
