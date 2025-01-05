8b9c3b58604b10e64d86edf383d2b61c8b0453ad
Egor Kotlyar <kotlaregor9@gmail.com>
2025-01-05 22:21:09 +0700

    Make the Deadfish Swim

--- script3.js
+++ script3.js
@@ -423,6 +423,34 @@
 // }
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// `
+// Create a parser to interpret and execute the Deadfish language.
+
+// Deadfish operates on a single value in memory, which is initially set to 0.
+
+// It uses four single-character commands:
+
+// i: Increment the value
+// d: Decrement the value
+// s: Square the value
+// o: Output the value to a result array
+// All other instructions are no-ops and have no effect.
+
+// Examples
+// Program "iiisdoso" should return numbers [8, 64].
+// Program "iiisdosodddddiso" should return numbers [8, 64, 3600].
+// `
+// function parse(data){
+//   let num = 0;
+//   const result = [];
+//   data.split("").forEach((el) => {
+//     if (el ===  "i") num += 1;
+//     if (el ===  "d") num -= 1;
+//     if (el ===  "s") num = num ** 2;
+//     if (el ===  "o") result.push(num);
+//   });
+//   return result;
+// }
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
