407a0dd0669331ce5292eca05e91835800f287fc
Egormity <kotlaregor9@gmail.com>
2025-01-14 16:26:10 +0700

    longestCommonPrefix

--- script3.js
+++ script3.js
@@ -650,6 +650,42 @@
 // console.log(romanToInt("MMMXLV"));
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// `
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
+// Example 1:
+
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+// Example 2:
+
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+
+// Constraints:
+
+// 1 <= strs.length <= 200
+// 0 <= strs[i].length <= 200
+// strs[i] consists of only lowercase English letters if it is non-empty.
+// `
+// /**
+//  * @param {string[]} arr
+//  * @return {string}
+//  */
+// var longestCommonPrefix = function (strs) {
+//     if (strs.length === 0) return "";
+
+//     let prefix = strs[0];
+//     for (let i = prefix.length - 1; i >= 0; i--) {
+//         if (strs.every((word) => word.startsWith(prefix))) return prefix;
+//         else prefix = prefix.slice(0, i);
+//     }
+
+//     return "";
+// };
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
