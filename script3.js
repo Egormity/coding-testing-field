5cac2406f4567c0293cba72bb37243253de61e17
Egormity <kotlaregor9@gmail.com>
2025-01-17 14:48:01 +0700

    searchInsert with binary search

--- script3.js
+++ script3.js
@@ -733,10 +733,72 @@
 // console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// `
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+// Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+// Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+// Return k.
+// `
+// /**
+//  * @param {number[]} nums
+//  * @param {number} val
+//  * @return {number}
+//  */
+// var removeElement = function (nums, val) {
+//     const sorted = nums.sort((a, b) => (a === val) - (b === val));
+//     sorted.forEach((el, i) => {
+//         nums[i] = el;
+//     });
+//     return nums.filter((el) => el === val).length;
+// };
+// const arr = [0, 1, 2, 2, 3, 0, 4, 2];
+// console.log(removeElement(arr, 2));
+// console.log(arr);
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// `
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+// `
+// /**
+//  * @param {string} haystack
+//  * @param {string} needle
+//  * @return {number}
+//  */
+// var strStr = function (haystack, needle) {
+//     return haystack.indexOf(needle);
+// };
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// `
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+// `
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number}
+//  */
+// var searchInsert = function (nums, target) {
+//     if (target < nums[0]) return 0;
+//     if (target > nums[nums.length - 1]) return nums.length;
+
+//     let left = 0;
+//     let right = nums.length - 1;
+//     let middle = Math.floor((left + right) / 2);
+
+//     while (nums[middle] !== target) {
+//         if (target < nums[middle]) right = middle - 1;
+//         else left = middle + 1;
+//         middle = Math.floor((left + right) / 2);
+
+//         if (left > right) return middle + 1;
+//     }
+
+//     return middle;
+// };
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
