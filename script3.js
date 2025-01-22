c075027edb0b41c4190f09c8ce8104667fa73285
Egormity <kotlaregor9@gmail.com>
2025-01-22 13:05:44 +0700

    climbStairs

--- script3.js
+++ script3.js
@@ -916,8 +916,34 @@
 // console.log(addBinary("11", "1"));
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// `
+// Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+// `;
+// /**
+//  * @param {number} x
+//  * @return {number}
+//  */
+// var mySqrt = function (x) {
+//     return Math.floor(Math.sqrt(x));
+// };
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// `
+// You are climbing a staircase. It takes n steps to reach the top.
+
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+// `;
+// /**
+//  * @param {number} n
+//  * @return {number}
+//  */
+// var climbStairs = function (n) {
+//     const fibs = [1, 2];
+//     for (let i = 2; i < n; i++) {
+//         fibs.push(fibs[i - 1] + fibs[i - 2]);
+//     }
+//     return fibs[n - 1];
+// };
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
