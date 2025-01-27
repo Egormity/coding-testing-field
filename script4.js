d56c5c5923f4473510156da9c2b83029d8729b50
Egormity <kotlaregor9@gmail.com>
2025-01-27 14:02:24 +0700

    addTwoNumbersReversedLinkedList

--- script4.js
+++ script4.js
@@ -1,4 +1,69 @@
+"use strict";
+
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// `
+// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+// `
+
+// /**
+//  * Definition for singly-linked list.
+//  * function ListNode(val, next) {
+//  *     this.val = (val===undefined ? 0 : val)
+//  *     this.next = (next===undefined ? null : next)
+//  * }
+//  */
+// /**
+//  * @param {ListNode} l1
+//  * @param {ListNode} l2
+//  * @return {ListNode}
+//  */
+
+// class ListNode {
+//     constructor(val, next) {
+//         this.val = val === undefined ? 0 : val;
+//         this.next = next === undefined ? null : next;
+//     }
+// }
+
+// var addTwoNumbers = function (l1, l2) {
+//     if (!l1 || (l1.val === 0 && isNaN(l1.next?.val))) return l2;
+//     if (!l2 || (l2.val === 0 && isNaN(l2.next?.val))) return l1;
+
+//     const resultList = new ListNode(null, null);
+//     let cur = resultList;
+//     let curl1 = l1;
+//     let curl2 = l2;
+
+//     let carry = 0;
+//     while (!isNaN(curl1?.val) || !isNaN(curl2?.val)) {
+//         let newNum = (curl1?.val || 0) + (curl2?.val || 0) + carry;
+//         carry = 0;
+
+//         if (newNum > 9) {
+//             carry = Math.floor(newNum / 10);
+//             newNum = newNum % 10;
+//         }
+
+//         if (cur.val === null) cur.val = newNum;
+//         else {
+//             cur.next = new ListNode(newNum, null);
+//             cur = cur.next;
+//         }
+
+//         curl1 = curl1?.next;
+//         curl2 = curl2?.next;
+//     }
+
+//     if (carry > 0) cur.next = new ListNode(carry, null);
+
+//     return resultList;
+// };
+
+// const l1 = new ListNode(2);
+// const l2 = new ListNode(0);
+// console.log(addTwoNumbers(l1, l2));
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -9,7 +74,7 @@
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-1233;
+
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
